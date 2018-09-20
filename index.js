ea7e32179b31fabfd0dec010d3c1c82a0b3489d2
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 01:39:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,3 +25,10 @@ class BoardMember{
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
   };
 }
+
+class Ceo extends BoardMember {
+  constructor(name, homeState) {
+    this.name = name;
+    this.homeState = homeState;
+  };
+}
