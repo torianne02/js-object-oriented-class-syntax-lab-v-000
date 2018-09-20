5ec0d77345371e5bffec7092677dc907eae36f2c
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-09-20 01:37:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,12 @@ class BoardMember{
   approve() {
     return "You can do that!"
   };
+
+  doCharity() {
+    return "I like to help people."
+  };
+
+  releasePressStatement() {
+    return "You will see great things from Scuber."
+  };
 }
